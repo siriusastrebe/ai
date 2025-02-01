5c3d11c680f697f0a7e5d7bce3473eff04704f12
sirius <siriusastrebe@gmail.com>
2025-01-31 16:13:22 -0800

    Explicit tokenizer and model name in sentiment analysis

--- huggingface/sentiment-analysis.py
+++ huggingface/sentiment-analysis.py
@@ -1,5 +1,11 @@
 # https://www.youtube.com/watch?v=QEaBAZQCtwE&t=239s
 from transformers import pipeline
-classifier = pipeline("sentiment-analysis")
-res = classifier("This ML stuff's really getting exciting huh")
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+res = classifier("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair")
 print(res)
